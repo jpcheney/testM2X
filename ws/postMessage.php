9cd7b3676fa3c080c3702c7b2b18f2b6dfc0b8b9
jcheney@free.fr <cheney01>
2016-06-06 09:59:22 +0200

    Nouvelle version de postMessage.php

--- ws/postMessage.php
+++ ws/postMessage.php
@@ -5,6 +5,41 @@ $deviceId = $_POST['deviceId'];
 $streamId = $_POST['streamId'];
 $valeur = $_POST['valeur'];
 
+//chargement de la librairie via l'autoload de composer
+require_once "../vendor/autoload.php";
+
+//declaration des objets
+use Att\M2X\M2X;
+use Att\M2X\Error\M2XException;
+
+//instanciation de l'objet
+$m2x = new M2X($apiKey);
+
+//Get the device
+$device = $m2x->device($deviceId);
+
+//Create the streams if they don't exist yet
+$device->updateStream($streamId);
+
+$now = date('c');
+$device->stream($streamId)->postValues(array(array('value' => $valeur,  'timestamp' => $now)));
+
+?>
+{
+	"reponse" : {
+		"code" : "OK",
+		"libelle" : "Tout s'est bien passe"
+	},
+	"valeurs": {
+		"apiKey" : "<?php echo $apiKey;?>",
+		"deviceId" : "<?php echo $deviceId;?>",
+		"streamId" : "<?php echo $streamId;?>",
+		"valeur" : "<?php echo $value;?>",
+		"timestamp" : "<?php echo $now;?>"
+	}
+}
+<?php
+/*
 $values = "";
 $values = $values . "apiKey=" . $apiKey . "\n";
 $values = $values . "deviceId=" . $deviceId . "\n";
@@ -14,4 +49,5 @@ $values = $values . "\n\n";
 
 
 $retour = file_put_contents("resultPostMessage.txt",$values,FILE_APPEND);
+*/
 ?>
