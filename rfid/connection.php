462939e46e7e51298da4654f52dc948caf5341aa
jcheney@free.fr <cheney01>
2016-06-08 10:28:04 +0200

    rfid

--- rfid/connection.php
+++ rfid/connection.php
@@ -3,5 +3,6 @@
 //mysql://bdd58b875d80b0:85d85ba1@eu-cdbr-west-01.cleardb.com/heroku_b43fd19e2d8e228?reconnect=true
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-echo $url;
+
+var_dump($url);
 ?>
